e48f5d239f329c4b88bd69f8b2c4eadccd9393ac
mahendarsingh1 <mahendarsinghrathod111@gmail.com>
2022-03-07 09:27:04 +0530

    filter section added

---
+++ src/components/subheader/FilterInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SearchBox(props){
+    return(
+        <div id={props.pid} className="inline filter-inputs">
+            {props.img && <img src={props.img} alt="" height="15px"/>}
+            {props.inputId && <input id={props.inputId} type={props.inputType} placeholder={props.inputPlaceholder}/>} 
+            {" "}
+            {props.text && props.text}
+        </div>
+    )
+}
+
+export default SearchBox;
