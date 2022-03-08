3611c6cfd25f6a855057b2bf17303ba73bc28f38
mahendarsingh1 <mahendarsinghrathod111@gmail.com>
2022-03-08 20:43:23 +0530

    moved img out of h4

--- src/components/subheader/DescriptionBox.js
+++ src/components/subheader/DescriptionBox.js
@@ -3,10 +3,8 @@ import React from "react";
 function DescriptionBox(props){
     return (
         <div id={props.id} className={props.classes}>
-            <h4>
-                {props.img && <img src={props.img} alt={props.heading}/>}
-                {props.heading}
-            </h4>
+            {props.img && <img src={props.img} alt={props.heading}/>}
+            <h4 className="inline">{props.heading}</h4>
             <p className="op-6">{props.paragraph}</p>
         </div>
     )
